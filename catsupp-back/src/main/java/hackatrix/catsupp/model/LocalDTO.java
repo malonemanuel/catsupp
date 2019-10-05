c9f44fb21c4ccb0ad5d9f0fb681122b852eeb882
Alvarez Iaconis Ezequiel <ezequiel.alvarez@supervielle.com.ar>
2019-10-05 12:45:54 -0300

    Test de resource get y find que responde con datos fijos

---
+++ catsupp-back/src/main/java/hackatrix/catsupp/model/LocalDTO.java
@@ -0,0 +1,12 @@
+package hackatrix.catsupp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LocalDTO {
+	private String id;
+	private String nombre;
+//	private List<PlatoDTO> platos;
+}
